0927ef7658b50c3b5341a8c0553e63f55ded940e
Tornike Razmadze <torniker@gmail.com>
2016-09-13 18:14:23 +0400

    posts and single post components

---
+++ ui/src/components/Post.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="post">
+    <div class="loading" v-if="loading">Loading...</div>
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+    <div v-if="post">
+      <h1>Blog</h1>
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false,
+      post: null,
+      error: null
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      this.error = this.post = null
+      this.loading = true
+      this.$http.get('post/' + this.$route.params.id)
+        .then((response) => {
+          this.loading = false
+          this.post = response.data
+        }, (response) => {
+          this.loading = false
+          this.error = response.data
+        })
+    }
+  }
+}
+</script>
---
+++ ui/src/components/Posts.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="posts">
+    <div class="loading" v-if="loading">Loading...</div>
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+    <div v-if="posts">
+      <h1>Blog</h1>
+      <ul>
+        <li v-for="post in posts">
+          <router-link :to="{ path: '/blog/'+post.id }">{{ post.title }}</router-link>
+        </li>
+      </ul>
+    </div>
+    <router-view class="child"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false,
+      posts: null,
+      error: null
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      this.error = this.posts = null
+      this.loading = true
+      this.$http.get('post')
+        .then((response) => {
+          this.loading = false
+          this.posts = response.data
+        }, (response) => {
+          this.loading = false
+          this.error = response.data
+        })
+    }
+  }
+}
+</script>
