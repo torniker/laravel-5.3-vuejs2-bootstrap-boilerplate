5294c37abe518953535de6084bf41a4573c5be92
Tornike Razmadze <torniker@gmail.com>
2016-09-13 18:17:10 +0400

    updated App component and main.js

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
+Vue.use(require('vue-resource'))
+var VueRouter = require('vue-router')
+Vue.use(VueRouter)
+var Config = require('./config.js')
+Vue.http.interceptors.push((request, next) => {
+  request.url = Config.apiurl() + request.url
+  next()
+})
+const routes = require('./routes.js')
+const router = new VueRouter(routes)
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: __dirname,
+//   routes: [
+//     { path: '/', component: Home },
+//     { path: '/shop', component: Products },
+//     { path: '/contact', component: Contact }
+//   ]
+// })
 import App from './App'
-
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
