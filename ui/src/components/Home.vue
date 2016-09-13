21e278ab8528847a71f5c6b62101070f3caba561
Tornike Razmadze <torniker@gmail.com>
2016-09-13 18:13:49 +0400

    home component

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -8,15 +8,8 @@
 export default {
   data () {
     return {
-      msg: 'Hello Vue!'
+      msg: 'Hello!'
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
-}
-</style>
