c60ed5c3e62c06cab90369a0ee17024a6cf1e1fb
Tornike Razmadze <torniker@gmail.com>
2016-09-13 18:13:07 +0400

    added some routes and dummy data

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,16 +1,30 @@
 <?php
+Route::get('laravel', function () {
+    return ['csrfToken' => csrf_token()];
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
- */
+Route::get('product', function () {
+    return [
+        ['id' => '123', 'name' => 'dummy product 1', 'price' => '1000'],
+        ['id' => '234', 'name' => 'dummy product 2', 'price' => '5'],
+        ['id' => '345', 'name' => 'dummy product 3', 'price' => '90151'],
+    ];
+});
 
-Route::get('/', function () {
-    return view('welcome');
+$posts = [
+    ['id' => '123', 'title' => 'blog post 1', 'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'],
+    ['id' => '234', 'title' => 'blog post 2', 'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'],
+    ['id' => '345', 'title' => 'blog post 3', 'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'],
+];
+
+Route::get('post', function () use ($posts) {
+    return $posts;
+});
+
+Route::get('post/{id}', function ($id) use ($posts) {
+    $key = array_search($id, array_column($posts, 'id'));
+    if (false !== $key) {
+        return $posts[$key];
+    }
+    return [];
 });
