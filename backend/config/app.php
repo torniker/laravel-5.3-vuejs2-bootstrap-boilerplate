1cc3057592ba543b72e088f3cae9a9e2486b36cc
Tornike Razmadze <torniker@gmail.com>
2016-09-13 11:20:06 +0400

    installed and configured laravel/passport

--- backend/config/app.php
+++ backend/config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
